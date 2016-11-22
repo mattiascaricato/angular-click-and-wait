a88ad19d640667886858ca1fafa5194de901e971
Matías Caricato <mattiascaricato@gmail.com>
2016-11-21 23:20:30 -0300

    Add directive

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('Hello World!');
+angular.module('clickAndWait', [])
+  .directive('clickAndWait', () => ({
+    restrict: 'A',
+    scope: {
+      asyncAction: '&clickAndWait',
+    },
+    link: (scope, element) => {
+      element.bind('click', () => {
+        element.prop('disabled', true);
+        scope.$apply(() => {
+          scope.asyncAction().finally(() => {
+            element.prop('disabled', false);
+          });
+        });
+      });
+    },
+  }));
